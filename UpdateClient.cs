d6ccfc43b55d522746b1a991f3b22d84e6b8dc16
clown <tt.clown@gmail.com>
2015-04-06 15:23:02 +0900

    GetMessageAsync を実装

--- UpdateClient.cs
+++ UpdateClient.cs
@@ -39,28 +39,6 @@ namespace Cube.Net
 
         /* ----------------------------------------------------------------- */
         ///
-        /// Uri
-        ///
-        /// <summary>
-        /// アップデート用 URL を取得または設定します。。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        public Uri Uri { get; set; }
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// MessageUri
-        ///
-        /// <summary>
-        /// アップデートメッセージ用 URL を取得または設定します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        public Uri MessageUri { get; set; }
-
-        /* ----------------------------------------------------------------- */
-        ///
         /// HttpHandler
         ///
         /// <summary>
@@ -95,7 +73,7 @@ namespace Cube.Net
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public Task RunAsync()
+        public Task RunAsync(Uri uri)
         {
             return Task.Factory.StartNew(() => { });
         }
@@ -109,22 +87,19 @@ namespace Cube.Net
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        public async Task<UpdateMessage> GetMessageAsync()
+        public async Task<UpdateMessage> GetMessageAsync(Uri uri)
         {
             try
             {
-                var response = await CreateHttpClient().GetAsync(MessageUri);
+                var response = await CreateHttpClient().GetAsync(uri);
                 if (!response.IsSuccessStatusCode) return null;
 
                 var encoding = System.Text.Encoding.GetEncoding("UTF-8"); // TODO: auto detect
                 using (var stream = await response.Content.ReadAsStreamAsync())
                 using (var reader = new System.IO.StreamReader(stream, encoding))
+                foreach (var m in CreateMessages(reader))
                 {
-                    for (var s = reader.ReadLine(); s != null; s = reader.ReadLine())
-                    {
-                        var message = CreateMessage(s);
-                        if (message.Version == Version) return message;
-                    }
+                    if (m.Version == Version) return m;
                 }
             }
             catch (Exception err) { System.Diagnostics.Trace.TraceError(err.ToString()); }
@@ -154,16 +129,87 @@ namespace Cube.Net
 
         /* ----------------------------------------------------------------- */
         ///
-        /// ParseMessage
+        /// CreateMessages
         ///
         /// <summary>
-        /// 文字列を解析して UpdateMessage オブジェクトを生成します。。
+        /// ストリームを解析して UpdateMessage オブジェクトを生成します。
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        private UpdateMessage CreateMessage(string str)
+        private IList<UpdateMessage> CreateMessages(System.IO.StreamReader reader)
         {
-            return new UpdateMessage();
+            var dest = new List<UpdateMessage>();
+
+            var version = string.Empty;
+            var args = new Dictionary<string, string>();
+
+            for (var l = reader.ReadLine(); !reader.EndOfStream; l = reader.ReadLine())
+            {
+                if (string.IsNullOrEmpty(l)) continue;
+
+                if (l[0] == '[' && l[l.Length - 1] == ']')
+                {
+                    var m = CreateMessage(version, args);
+                    if (m != null) dest.Add(m);
+                    version = l.Substring(1, l.Length - 2);
+                    args.Clear();
+                }
+                else Split(l, args);
+            }
+
+            return dest;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// CreateMessage
+        ///
+        /// <summary>
+        /// 指定されたバージョンと引数から UpdateMessage オブジェクトを
+        /// 生成します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private UpdateMessage CreateMessage(string version, IDictionary<string, string> args)
+        {
+            if (string.IsNullOrEmpty(version) || args.Count <= 0) return null;
+
+            try
+            {
+                var update = int.Parse(args["UPDATE"]);
+                return new UpdateMessage
+                {
+                    Version = version,
+                    Notify  = (update == 1),
+                    Text    = args["MESSAGE"],
+                    Uri     = new Uri(args["URL"])
+                };
+            }
+            catch (Exception err) { System.Diagnostics.Trace.TraceError(err.ToString()); }
+
+            return null;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Split
+        ///
+        /// <summary>
+        /// 文字列を KeyValuePair に分割して追加します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void Split(string src, IDictionary<string, string> dest)
+        {
+            if (string.IsNullOrEmpty(src)) return;
+
+            var pos = src.IndexOf('=');
+            if (pos < 0) return;
+
+            var key = src.Substring(0, pos);
+            var value = src.Substring(pos + 1, src.Length - (pos + 1));
+            if (dest.ContainsKey(key)) dest[key] = value;
+            else dest.Add(key, value);
         }
 
         #endregion
