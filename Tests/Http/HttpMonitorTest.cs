909e6a42ac771de34293a4bc99acf789babacd27
clown <tt.clown@gmail.com>
2017-06-06 21:18:39 +0900

    Update libraries

--- Tests/Http/HttpMonitorTest.cs
+++ Tests/Http/HttpMonitorTest.cs
@@ -17,7 +17,6 @@
 /* ------------------------------------------------------------------------- */
 using System;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
