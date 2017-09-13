787a8faaedfe293aa6b2ef53a1a917b21110fc7f
clown <tt.clown@gmail.com>
2017-09-13 22:09:19 +0900

    Fix for monitoring RSS feeds.

--- Applications/Rss.Reader/Models/RssFacade.cs
+++ Applications/Rss.Reader/Models/RssFacade.cs
@@ -15,8 +15,10 @@
 /// limitations under the License.
 ///
 /* ------------------------------------------------------------------------- */
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Cube.Net.Rss;
 using Cube.Settings;
 
 namespace Cube.Net.Applications.Rss.Reader
@@ -49,8 +51,16 @@ namespace Cube.Net.Applications.Rss.Reader
             if (System.IO.File.Exists(filename))
             {
                 var src = SettingsType.Json.Load<List<RssCategory>>(filename);
-                foreach (var item in src) Categories.Add(item);
+                foreach (var item in src)
+                {
+                    Categories.Add(item);
+                    Add(item);
+                }
             }
+
+            Monitor.Interval = TimeSpan.FromHours(1);
+            Monitor.Subscribe(WhenReceived);
+            Monitor.Start();
         }
 
         #endregion
@@ -68,6 +78,64 @@ namespace Cube.Net.Applications.Rss.Reader
         /* ----------------------------------------------------------------- */
         public ObservableCollection<RssCategory> Categories { get; }
             = new ObservableCollection<RssCategory>();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Feeds
+        /// 
+        /// <summary>
+        /// RSS フィード購読サイトおよびカテゴリ一覧を取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public IDictionary<Uri, RssFeed> Feeds { get; }
+            = new Dictionary<Uri, RssFeed>();
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Monitor
+        /// 
+        /// <summary>
+        /// RSS フィード監視用オブジェクトを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public RssMonitor Monitor { get; } = new RssMonitor();
+
+        #endregion
+
+        #region Implementations
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Add
+        /// 
+        /// <summary>
+        /// RSS フィードを RssMonitor オブジェクトに登録します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void Add(RssCategory category)
+        {
+            foreach (var f in category.Feeds) Monitor.Uris.Add(f.Feed);
+            if (category.Categories == null) return;
+            foreach (var c in category.Categories) Add(c);
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// WhenReceived
+        /// 
+        /// <summary>
+        /// RSS フィードを RssMonitor オブジェクトに登録します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private void WhenReceived(Uri uri, RssFeed feed)
+        {
+            if (Feeds.ContainsKey(uri)) Feeds[uri] = feed;
+            else Feeds.Add(uri, feed);
+        }
 
         #endregion
     }
