4c85b08f01ebb8e0d45731a21c2e45a8b3998892
clown <tt.clown@gmail.com>
2018-02-08 20:29:00 +0900

    Remove unused statements.

--- Applications/Rss/Tests/Interactions/SimplexConverterTest.cs
+++ Applications/Rss/Tests/Interactions/SimplexConverterTest.cs
@@ -16,9 +16,6 @@
 //
 /* ------------------------------------------------------------------------- */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Cube.Settings;
 using System.Globalization;
 using System.Windows;
 using Cube.Net.Rss;
